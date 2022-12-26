24261298b4bdaa57805dada1dc57d0ea8eb46102
FlorencioQuelca <florens.lonwe@gmail.com>
2022-12-26 19:04:54 -0400

    finish

--- src/auth/auth.services.js
+++ src/auth/auth.services.js
@@ -6,12 +6,12 @@ const mailer = require('../utils/mailer')
 const config = require('../../config')
 
 const postLogin = (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
-    if(email && password){
+    if (email && password) {
         authControllers.checkUsersCredentials(email, password)
             .then((data) => {
-                if(data){
+                if (data) {
                     const token = jwt.sign({
                         id: data.id,
                         email: data.email,
@@ -23,60 +23,63 @@ const postLogin = (req, res) => {
                         token
                     })
                 } else {
-                    res.status(401).json({message: 'Invalid Credentials'})
+                    res.status(401).json({ message: 'Invalid Credentials' })
                 }
             })
             .catch((err) => {
-                res.status(400).json({message: err.message})
+                res.status(400).json({ message: err.message })
             })
     } else {
-        res.status(400).json({message: 'Missing Data', fields: {
-            email: 'example@example.com',
-            password: "string"
-        }})
+        res.status(400).json({
+            message: 'Missing Data',
+            fields: {
+                email: 'example@example.com',
+                password: "string"
+            }
+        })
     }
 }
 
 const postRecoveryToken = (req, res) => {
 
-    const { email } = req.body     
-    if(email){
+    const { email } = req.body
+    if (email) {
         authControllers.createRecoveryToken(email)
-        .then((data) => {
-            if(data){
-                mailer.sendMail({
-                    from: '<test.academlo@gmail.com>',
-                    to: email,
-                    subject: 'Recuperación de Contraseña',
-                    html: `<a href='${config.api.host}/api/v1/auth/recovery-password/${data.id}'>${config.api.host}/api/v1/auth/recovery-password/${data.id}</a>`
-                })
-            }
-            
-            res.status(200).json({message: 'Email sended!, Check your inbox'})
+            .then((data) => {
+                if (data) {
+                    mailer.sendMail({
+                        from: '<florens.lonwe@gmail.com>',
+                        to: email,
+                        subject: 'Recuperación de Contraseña',
+                        html: `<a href='${config.api.host}/api/v1/auth/recovery-password/${data.id}'>${config.api.host}/api/v1/auth/recovery-password/${data.id}</a>`
+                    })
+                }
 
-        })
-        .catch((err) => {
-            res.status(400).json({message: err.message})
-        })
+                res.status(200).json({ message: 'Email sended!, Check your inbox' })
+
+            })
+            .catch((err) => {
+                res.status(400).json({ message: err.message })
+            })
     } else {
-        res.status(400).json({message: 'Invalid Data', fields: {email: 'example@example.com'}})
+        res.status(400).json({ message: 'Invalid Data', fields: { email: 'example@example.com' } })
     }
 }
 
 const patchPassword = (req, res) => {
     const id = req.params.id //? es el id del registro de recoveryPassword (para recuperar la contraseña)
     const { password } = req.body
-      
+
     authControllers.changePassword(id, password)
         .then(data => {
-            if(data){
-                res.status(200).json({message: 'Password updated succesfully!'})
+            if (data) {
+                res.status(200).json({ message: 'Password updated succesfully!' })
             } else {
-                res.status(400).json({message: 'URL expired'})
+                res.status(400).json({ message: 'URL expired' })
             }
         })
         .catch(err => {
-            res.status(400).json({message: err.message})
+            res.status(400).json({ message: err.message })
         })
 }
 
